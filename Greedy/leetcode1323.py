c5869954541897086786d51c1f65a3d1a25de1ea
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2023-04-05 20:54:49 +0900

    230405

---
+++ Greedy/leetcode1323.py
@@ -0,0 +1,19 @@
+# 1323. Maximum 69 Number
+
+"""
+You are given a positive integer num consisting only of digits 6 and 9.
+
+Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
+"""
+
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        result = []
+        flag = 0
+        for i in str(num):
+            if i == '6' and flag == 0:
+                result.append('9')
+                flag = 1
+            else:
+                result.append(i)
+        return int(''.join(result))
