e4d7c2b71a9230995d01e8de7e894eabb675135a
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2023-04-12 16:30:51 +0900

    230412

---
+++ Greedy/leetcode1710.py
@@ -0,0 +1,39 @@
+# LeetCode 1710. Maximum Units on a Truck
+
+"""
+You are assigned to put some amount of boxes onto one truck. You are given a 2D array boxTypes,
+where boxTypes[i] = [numberOfBoxesi, numberOfUnitsPerBoxi]:
+
+numberOfBoxesi is the number of boxes of type i.
+numberOfUnitsPerBoxi is the number of units in each box of the type i.
+You are also given an integer truckSize, which is the maximum number of boxesthat can be puton the truck.
+You can choose any boxes to put on the truck as long as the number of boxes does not exceed truckSize.
+
+Return the maximum total number of units that can be put on the truck.
+
+Input: boxTypes = [[1,3],[2,2],[3,1]], truckSize = 4
+Output: 8
+Explanation: There are:
+- 1 box of the first type that contains 3 units.
+- 2 boxes of the second type that contain 2 units each.
+- 3 boxes of the third type that contain 1 unit each.
+You can take all the boxes of the first and second types, and one box of the third type.
+The total number of units will be = (1 * 3) + (2 * 2) + (1 * 1) = 8.
+
+Input: boxTypes = [[5,10],[2,5],[4,7],[3,9]], truckSize = 10
+Output: 91
+"""
+
+class Solution:
+    def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        boxTypes.sort(key=lambda x: x[1], reverse=True)
+        result = 0
+
+        for box, unit in boxTypes:
+            if box <= truckSize:
+                result += box * unit
+                truckSize -= box
+            else:
+                result += truckSize * unit
+                break
+        return result
