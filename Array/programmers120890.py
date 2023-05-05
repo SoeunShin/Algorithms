302935e3949a84e55b5140e32f034b2387e789ad
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2023-05-05 23:57:31 +0900

    230505

---
+++ Array/programmers120890.py
@@ -0,0 +1,15 @@
+# Programmers 120890. 가까운 수
+
+"""
+정수 배열 array와 정수 n이 매개변수로 주어질 때, array에 들어있는 정수 중 n과 가장 가까운 수를 return 하도록 solution 함수를 완성해주세요.
+
+입출력 예 #1
+3, 10, 28 중 20과 가장 가까운 수는 28입니다.
+
+입출력 예 #2
+10, 11, 12 중 13과 가장 가까운 수는 12입니다.
+"""
+
+def solution(array, n):
+    minus = []
+    return answer
