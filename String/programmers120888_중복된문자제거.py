718e284e3c950cdd76e45a3d0123f61e10dac4a8
Soeun Shin <61735948+SoeunShin@users.noreply.github.com>
2023-06-04 21:57:54 +0900

    230604

---
+++ String/programmers120888_중복된문자제거.py
@@ -0,0 +1,16 @@
+# Programmers 120888. 중복된 문자 제거
+
+"""
+문자열 my_string이 매개변수로 주어집니다. my_string에서 중복된 문자를 제거하고 하나의 문자만 남긴 문자열을 return하도록 solution 함수를 완성해주세요.
+
+my_string	result
+"people"	"peol"
+"We are the world"	"We arthwold"
+"""
+
+def solution(my_string):
+    answer = ''
+    for c in my_string:
+        if c not in answer:
+            answer += c
+    return answer
